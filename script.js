d73bbd2f4a251ade137b6159e88384706723073c
adityaK006 <120311231+adityaK006@users.noreply.github.com>
2023-07-02 20:28:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const emptySlotsCountElement = document.querySelector('.empty-slots-count');
 const parkingSlotsTab = document.getElementById('parkingSlotsTab');
-const manageSlotsTab = document.getElementById('manageSlotsTab');
 const manageSlotsContainer = document.getElementById('manageSlotsContainer');
+const goToHomeLink = document.getElementById('goToHomeLink');
 
 let emptySlotsCount = 40; // Update the initial count to 40
 const totalSlots = 40; // Update the total slots to 40
-const bookedSlots = [];
+let bookedSlots = [];
 
 function toggleBooking(slot) {
   const element = document.getElementById(slot);
@@ -25,6 +25,7 @@ function toggleBooking(slot) {
 
   element.classList.toggle('selected');
   updateEmptySlotsCount();
+  saveBookedSlots(); // Save booked slots to localStorage
 }
 
 function updateEmptySlotsCount() {
@@ -32,19 +33,25 @@ function updateEmptySlotsCount() {
 }
 
 function showManageSlots() {
-  parkingSlotsTab.style.display = 'none';
-  manageSlotsTab.style.display = 'block';
-  manageSlotsContainer.classList.add('active');
-  document.querySelector('.empty-slots-tab').style.display = 'none';
-  document.querySelector('.slots-container').style.display = 'grid';
+  manageSlotsContainer.style.display = 'grid';
+  updateEmptySlotsCount();
+  goToHomeLink.style.display = 'inline-block'; // Show the Go to Home link
+  parkingSlotsTab.removeEventListener('click', showManageSlots); // Remove the event listener for the Parking Slots tab
+  parkingSlotsTab.addEventListener('click', showParkingSlots); // Add the event listener for the Parking Slots tab to switch back
 }
 
 function showParkingSlots() {
-  parkingSlotsTab.style.display = 'block';
-  manageSlotsTab.style.display = 'none';
-  manageSlotsContainer.classList.remove('active');
-  document.querySelector('.empty-slots-tab').style.display = 'flex';
-  document.querySelector('.slots-container').style.display = 'none';
+  manageSlotsContainer.style.display = 'none';
+  updateEmptySlotsCount();
+  goToHomeLink.style.display = 'none'; // Hide the Go to Home link
+  parkingSlotsTab.removeEventListener('click', showParkingSlots); // Remove the event listener for the Parking Slots tab
+  parkingSlotsTab.addEventListener('click', showManageSlots); // Add the event listener for the Parking Slots tab to switch back
+}
+
+function showHome() {
+  manageSlotsContainer.style.display = 'none';
+  updateEmptySlotsCount();
+  goToHomeLink.style.display = 'none'; // Hide the Go to Home link
 }
 
 // Create slots dynamically
@@ -59,4 +66,28 @@ for (let i = 1; i <= totalSlots; i++) {
   slotsContainer.appendChild(slot);
 }
 
+function loadBookedSlots() {
+  const storedBookedSlots = localStorage.getItem('bookedSlots');
+  if (storedBookedSlots) {
+    bookedSlots = JSON.parse(storedBookedSlots);
+    emptySlotsCount = totalSlots - bookedSlots.length;
+
+    bookedSlots.forEach((slot) => {
+      const element = document.getElementById(slot);
+      if (element) {
+        element.classList.add('booked');
+        element.classList.add('selected');
+      }
+    });
+  }
+}
+
+function saveBookedSlots() {
+  localStorage.setItem('bookedSlots', JSON.stringify(bookedSlots));
+}
+
+loadBookedSlots();
 updateEmptySlotsCount();
+
+parkingSlotsTab.addEventListener('click', showManageSlots);
+goToHomeLink.addEventListener('click', showHome);
